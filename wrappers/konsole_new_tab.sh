48a68ae90510dbb1c9c7b1e65647251481823648
ps-aux <seriouz.accz@gmail.com>
2014-12-08 22:28:47 +0100

    Added Konsole wrapper for opening new tab and getting focus

---
+++ wrappers/konsole_new_tab.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#Opens the given directory in Konsole terminal
+#and brings the Konsole window to the foreground.
+#The windows created before Konsole window must 
+#not contain word "konsole" in them (like for example
+#Vim editing this file:)
+
+konsole  --new-tab --workdir  $1
+wmctrl -a "konsole"
+
